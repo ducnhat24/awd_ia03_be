0f3430c1d346a94bb6c64127bc14ed4afb4fe18c
ducnhat24 <ducnhaat@gmail.com>
2025-11-02 15:04:33 +0700

    config for Rendering

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,9 @@ import { UserModule } from './user/user.module';
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        ssl: configService.get<string>('DB_SSL') === 'true'
+          ? { rejectUnauthorized: false }
+          : false,
         synchronize: true,
       }),
     }),
